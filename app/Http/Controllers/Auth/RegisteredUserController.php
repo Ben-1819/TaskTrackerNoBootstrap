13d7c52ea85d7183a427ce0360a282e7de50eeab
Ben-1819 <ben.brown20332@gmail.com>
2025-03-27 14:49:54 +0000

    feat: Added profile picture functionality

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -8,6 +8,7 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
@@ -33,12 +34,25 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            "avatar" => ["image"],
         ]);
+
+        log::info("Check if the user uploaded their own profile picture");
+        if($request->avatar === null){
+            log::info("User has not uplaoded their own profile picture, assigning them a default profile picture");
+            $avatarName = "1743069891.png";
+        }
+        else{
+            log::info("User has uploaded their own profile picture");
+            $avatarName = time(). '.' .$request->avatar->getClientOriginalExtension();
+            $request->avatar->move(public_path("avatars"), $avatarName);
+        }
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            "avatar" => $avatarName,
         ]);
 
         event(new Registered($user));
