592d53b4e34927f1400cdab5aec92b6ade07e3f1
Ben-1819 <ben.brown20332@gmail.com>
2025-03-27 14:23:08 +0000

    feat: Created guest navbar and added it to guest layout

---
+++ resources/views/layouts/guest-navbar.blade.php
@@ -0,0 +1,7 @@
+<nav>
+    <div class="nav-items">
+        <a class="homeBtn" href="{{route("welcome")}}">Home</a>
+        <a href="{{route("login")}}">Login</a>
+        <a id="registerLink" href="{{route("register")}}">Register</a>
+    </div>
+</nav>
